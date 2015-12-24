03fd68712270eb7fab9511180e2f9f9a46ac7a5d
Admin <admin@macintosh.local>
2015-12-24 10:52:10 +0200

    Initial Commit

---
+++ Expense Maneger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Expense Maneger
+//
+//  Created by Admin on 12/24/15.
+//  Copyright © 2015 elena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
