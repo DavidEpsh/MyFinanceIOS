3a0371d604516afe5f9ec5cd42af0e97f167aab3
David <davidep1@gmail.com>
2016-01-12 12:54:09 +0200

    minor changes

---
+++ Expense Maneger/ValidateEmail.m
@@ -0,0 +1,13 @@
+//
+//  ValidateEmail.m
+//  Expense Maneger
+//
+//  Created by David on 1/12/16.
+//  Copyright © 2016 elena. All rights reserved.
+//
+
+#import "ValidateEmail.h"
+
+@implementation ValidateEmail
+
+@end
