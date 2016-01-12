2a768c5b55bf81a0f62cf124e32dc7ce23afac50
Admin <admin@Macintosh.local>
2016-01-12 23:26:37 +0200

    jhg

---
+++ Expense Maneger/TripsTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TripsTableViewCell.m
+//  Expense Maneger
+//
+//  Created by Admin on 1/12/16.
+//  Copyright © 2016 elena. All rights reserved.
+//
+
+#import "TripsTableViewCell.h"
+
+@implementation TripsTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
