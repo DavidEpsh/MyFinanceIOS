47822a17ddc0b855440f3442f7ad0eb20ce69ca4
Admin <admin@Macintosh.local>
2016-01-13 00:01:00 +0200

    fsdfs

---
+++ Expense Maneger/HomeTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HomeTableViewCell.h
+//  Expense Maneger
+//
+//  Created by Admin on 1/12/16.
+//  Copyright © 2016 elena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeTableViewCell : UITableViewCell
+
+@end
---
+++ Expense Maneger/HomeTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  HomeTableViewCell.m
+//  Expense Maneger
+//
+//  Created by Admin on 1/12/16.
+//  Copyright © 2016 elena. All rights reserved.
+//
+
+#import "HomeTableViewCell.h"
+
+@implementation HomeTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
